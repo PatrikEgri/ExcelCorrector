6a5aed5a3b62935761c47f1eecb3845ee26a78f0
Patrik Egri <55623475+PatrikEgri@users.noreply.github.com>
2020-03-14 19:55:45 +0100

    Single file correction is working

--- Models/Corrector.cs
+++ Models/Corrector.cs
@@ -1,10 +1,105 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ExcelCorrector.Models
 {
-    static class Corrector
+    public class Corrector
     {
+        /// <summary>
+        /// The file to be corrected.
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// The directory where the result of correction will be findable.
+        /// </summary>
+        public string PathToSave { get; set; }
+
+        /// <summary>
+        /// The correction key that has the conditions to the correction.
+        /// </summary>
+        public List<Models.Key> CorrectionKey { get; set; }
+
+        /// <summary>
+        /// Stores the earned points per key.
+        /// </summary>
+        public Dictionary<string, float> EarnedPoints { get; set; }
+
+        /// <summary>
+        /// Initializes the properties.
+        /// </summary>
+        /// <param name="filename">File to be corrected</param>
+        /// <param name="correctionKey">Correction key that has the conditions to the correction</param>
+        /// <param name="pathToSave">Directory where the result of correction will be findable</param>
+        public Corrector(string filename, List<Models.Key> correctionKey, string pathToSave)
+        {
+            Filename = filename;
+            PathToSave = pathToSave;
+            CorrectionKey = correctionKey;
+            EarnedPoints = new Dictionary<string, float>();
+        }
+
+        /// <summary>
+        /// Corrects the file to be corrected by the conditions of CorrectionKey and stores the results.
+        /// </summary>
+        public void CorrectSingleFile()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(new FileInfo(Filename)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+
+                foreach (var x in CorrectionKey)
+                {
+                    EarnedPoints.Add(x.Name, 0F);
+                    foreach (var y in x.Conditions)
+                    {
+                        switch (y.Type)
+                        {
+                            case ConditionTypes.Containment:
+                                EarnedPoints[x.Name] += worksheet.Cells[x.CalculateCoordinateFromIndexes()].Formula.Contains(y.Expression.ToUpper()) ? y.Points : 0F;
+                                break;
+
+                            case ConditionTypes.Equivalence:
+                                EarnedPoints[x.Name] += worksheet.Cells[x.CalculateCoordinateFromIndexes()].Value.ToString().Equals(y.Expression) ? y.Points : 0F;
+                                break;
+
+                            default: throw new Exception();
+                        }
+                    }
+                }
+            }
+            WriteSingeFileCorrectionResult();
+        }
+
+        /// <summary>
+        /// Writes the results of correction into a new Excel workbook.
+        /// </summary>
+        void WriteSingeFileCorrectionResult()
+        {
+            string savepath = $@"{ PathToSave }\result.xlsx";
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(new FileStream(savepath, FileMode.Create)))
+            {
+                package.Workbook.Worksheets.Add("Result");
+                var worksheet = package.Workbook.Worksheets["Result"];
+
+                byte columnIndex = 65;
+                foreach (var x in EarnedPoints)
+                {
+                    worksheet.Cells[$"{(char)columnIndex}1"].Value = x.Key;
+                    worksheet.Cells[$"{(char)columnIndex}2"].Value = x.Value;
+                    columnIndex++;
+                }
+                worksheet.Cells[$"{(char)columnIndex}2"].Value = $"{ 100 / CorrectionKey.Sum(x => x.Conditions.Sum(y => y.Points)) * EarnedPoints.Sum(x => x.Value) } %";
+
+                package.Save();
+            }
+        }
     }
 }
--- Pages/SingleFileCorrector.xaml.cs
+++ Pages/SingleFileCorrector.xaml.cs
@@ -14,10 +14,29 @@ namespace ExcelCorrector.Pages
     /// </summary>
     public partial class SingleFileCorrector : Page, INotifyPropertyChanged
     {
+        /// <summary>
+        /// An event that will be invoked when a property is changed.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// The file to be corrected.
+        /// </summary>
         string _filename;
+
+        /// <summary>
+        /// The directory where the result of correction will be findable.
+        /// </summary>
+        string _pathToSave;
+
+        /// <summary>
+        /// The file to be corrected will be corrected by this correction key.
+        /// </summary>
         List<Models.Key> _correctionKey;
+
+        /// <summary>
+        /// List of correction keys.
+        /// </summary>
         List<string> _files;
 
         /// <summary>
@@ -25,6 +44,9 @@ namespace ExcelCorrector.Pages
         /// </summary>
         readonly string _dir = $@"{ AppDomain.CurrentDomain.GetData("DataDirectory").ToString() }\";
 
+        /// <summary>
+        /// Property to handle easily _files.
+        /// </summary>
         public List<string> Files
         {
             get => _files;
@@ -37,6 +59,10 @@ namespace ExcelCorrector.Pages
                 }
             }
         }
+
+        /// <summary>
+        /// Property to handle easily _filename.
+        /// </summary>
         public string Filename
         {
             get => _filename;
@@ -49,6 +75,26 @@ namespace ExcelCorrector.Pages
                 }
             }
         }
+
+        /// <summary>
+        /// Property to handle easily _pathToSave.
+        /// </summary>
+        public string PathToSave
+        {
+            get => _pathToSave;
+            set
+            {
+                if (value != _pathToSave)
+                {
+                    _pathToSave = value;
+                    OnPropertyChanged("PathToSave");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Property to handle easily _correctionKey.
+        /// </summary>
         public List<Models.Key> CorrectionKey
         {
             get => _correctionKey;
@@ -62,6 +108,9 @@ namespace ExcelCorrector.Pages
             }
         }
 
+        /// <summary>
+        /// Initializes and lists files.
+        /// </summary>
         public SingleFileCorrector()
         {
             DataContext = this;
@@ -70,6 +119,9 @@ namespace ExcelCorrector.Pages
             ListFiles();
         }
 
+        /// <summary>
+        /// Fills the ListView with name of correction keys.
+        /// </summary>
         void ListFiles()
         {
             Files.Clear();
@@ -79,16 +131,28 @@ namespace ExcelCorrector.Pages
                 Files.Add(x.Replace(_dir, "").Replace(".xml", ""));
         }
 
+        /// <summary>
+        /// Deserializes the specified correction key.
+        /// </summary>
+        /// <param name="file"></param>
         void ReadFile(string file)
         {
             CorrectionKey = Serializer.Load(file);
         }
 
+        /// <summary>
+        /// Invokes PropertyChanged event with the specified string.
+        /// </summary>
+        /// <param name="propertyName">The name of property that called this method</param>
         void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// Opens FileDialog to set FileName.
+        /// </summary>
+        /// <param name="sender">The Button that triggered this event</param>
         void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
@@ -97,6 +161,10 @@ namespace ExcelCorrector.Pages
                 Filename = fileDialog.FileName;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">The ListView that triggered this event</param>
         private void lsvKeys_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (lsvKeys.SelectedItems != null)
@@ -105,6 +173,35 @@ namespace ExcelCorrector.Pages
                 btnCorrect.IsEnabled = true;
             }
             else btnCorrect.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Requests the correction.
+        /// </summary>
+        /// <param name="sender">The Button that triggered this event</param>
+        private void btnCorrect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                new Corrector(Filename, CorrectionKey, PathToSave).CorrectSingleFile();
+                MessageBox.Show("Sikeres javítás!");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        /// <summary>
+        /// Opens FileDialog to set PathToSave.
+        /// </summary>
+        /// <param name="sender">The Button that triggered this event</param>
+        private void btnSetSavePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if (fileDialog.ShowDialog() == true)
+                PathToSave = fileDialog.FileName.Remove(fileDialog.FileName.LastIndexOf('\\'));
         }
     }
 }
